0764214395d35898b1c245665e2114e69794a44d
OlivierRecher <recher.olivier@outlook.fr>
2025-06-19 10:53:18 +0200

    feat(): add dotenv

--- utils/mongoConnection.js
+++ utils/mongoConnection.js
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
 
 const mongoConnection = async () => {
   return new Promise((resolve, reject) => {
     mongoose
-      .connect("mongodb://inesPrime:lesinesdufonddelaclasse@127.0.0.1:28017/")
+      .connect(process.env.MONGO_PATH)
       .then(() => {
         resolve(null);
       })
