1317b6f3efd01133afda2e6819b8849beef3bff4
Ian Bertin <ian.bertin2004@gmail.com>
2026-01-19 20:47:45 +0100

    Users' schema

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
+  username: String,
   password: String,
   passwordChangedDate:Date,
   registrationDate:Date,
+  registerToken:String,
   verificationDate:Date,
-  verificationToken:String,
   isAdmin:Boolean,
 });
 
